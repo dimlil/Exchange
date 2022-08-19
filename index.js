7e4d099c8191aa76959dbc65bb101bfc992a9547
dimlil <d.v.vassilev@gmail.com>
2022-08-19 17:12:28 +0300

    feat/ created dropdown and get value for selected currencie

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-console.log('hey');
+const exchange = document.getElementById('exchange').value;
+console.log(exchange);
 
 async function getExchange() {
     const result = await fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/usd.json')
-            .then((response) => response.json())
-            .then((data) => { return data });
+        .then((response) => response.json())
+        .then((data) => { return data });
     console.log(result);
 }
 
