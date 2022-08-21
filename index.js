fccbe5cfbd183c181700f00d5703fe0f520459dd
dimlil <d.v.vassilev@gmail.com>
2022-08-21 22:53:09 +0300

    feat/ get only needed currencies

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ let exchange = document.getElementById('exchange').value;
 
 document.getElementById('exchange').addEventListener('change', () => {
     exchange = document.getElementById('exchange').value;
-    console.log(exchange);
-})
-
-console.log(exchange);
+    getExchange();
+});
 
 async function getExchange() {
-    const result = await fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/usd.json')
+    const result = await fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json')
         .then((response) => response.json())
-        .then((data) => { return data });
-    console.log(result);
+        .then((data) => { return data.usd });
+    getNeededCurrencies(result);
+}
+
+function getNeededCurrencies(obj) {
+    Object.entries(obj).forEach(entry => {
+        const [key, value] = entry;
+        if (key === "usd" || key === "eur" || key === "aud" || key === "cad" || key === "chf" || key === "nzd" || key === "bgn") {
+            if (key !== exchange.toLowerCase()) {
+                console.log(key, value);
+            }
+        }
+    });
 }
 
 getExchange();
